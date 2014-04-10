6d56c82be4baab783aad1687fe3c33d70316e25c
Mateusz Kulewicz <matikula9@gmail.com>
2014-04-10 20:25:38 +0200

    More geometry queries

--- sql/geometry.sql
+++ sql/geometry.sql
@@ -13,3 +13,23 @@ SELECT 'Distance - 2 points', ST_Distance(
     ST_GeomFromText('POINT(-0.5 -0.5)'),
     ST_GeomFromText('POINT(0.5 0.5)')
 );
+
+-- distance between point and a line
+
+-- should be 1
+SELECT 'Distance between point and a line', ST_Distance(
+    ST_GeomFromText('LINESTRING(1 0, 1 1)'),
+    ST_GeomFromText('POINT(0 0)')
+);
+
+-- should be sqrt(2)
+SELECT 'Distance between point and a line', ST_Distance(
+    ST_GeomFromText('LINESTRING(1 1, 1 2)'),
+    ST_GeomFromText('POINT(0 0)')
+);
+
+-- do two lines intersect? other syntax, ::geometry
+SELECT 'Do two lines intersect?', st_Intersects(
+    'LINESTRING(-10 0, 10 0)'::geometry, 
+    'LINESTRING(0 -10, 0 10)'::geometry
+);
